923118aed5d8f07cc6c7602c51a87ae3ea6d6324
cyan1470 <176188636@qq.com>
2015-07-30 09:59:52 +0800

    update filename

--- filenametags.sh
+++ filenametags.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+cscope -Rbq
+echo cscope -Rbq Done
+ctags -R
+echo ctags -R Done
 echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/"> filenametags
 find . -not -regex '.*\.\(png\|gif\)'  ! -path "*svn*" -type f -printf "%f\t%p\t1\n" | sort -f>> filenametags
